265dc6b21581ef0197ad05ba6bded43fccb7a17f
Byron-Hans <37991863+Byron-Hans@users.noreply.github.com>
2019-04-21 03:12:17 -0300

    agora consegui arrumar o bagui de parar o jogo quando ele morre e já criei as variaveis pra acabar com o jogo seja por vitória ou derrota, arrumei identenção e coloquei uns espaçamentos pra ficar mais legível e mais algumas coisas que eu esqueci agora

--- ep1.py
+++ ep1.py
@@ -22,34 +22,60 @@ cenário = {
 
 
 #início do código do jogo
+
+jogo = True #o jogo funciona enquanto for true 
+derrota = "GAME OVER! x__x "
+Vitória = "Parabéns! Você venceu! Agora vá fazer o EP1! ¯\_(ツ)_/¯"
+
 while jogo == True:
-    print("KITARA: Olá Insperiano, meu nome é Kitara, sou um espectro invisível vindo de"
-      " outra de dimensão. Fui enviada para porque você precisa conseguir"
-      " adiar a entrega do EP1 de Design de Software senão você e todos os"
-      " seus colegas vão pegar DP nessa matéria!!! Ficarei com você até"
-      " conseguir, só então poderei voltar para minha dimensão!")
+    
+    print("KITARA: Olá Insperiano, meu nome é Kitara, sou um espectro"
+      " invisível vindo de outra de dimensão. Fui enviada para porque você"
+      " precisa conseguir adiar a entrega do EP1 de Design de Software senão"
+      " senão você e todos os seus colegas vão pegar DP nessa matéria!!!"
+      " Ficarei com você até conseguir, só então poderei voltar para minha"
+      "dimensão!")
+    
     print("")
+    
     print(cenário["Prédio 1"])
+    
     opção_1 = "A: Sair correndo com medo da Kitara."
     opção_2 = "B: Pedir para Kitara explicar melhor quem ela é."
+    
     print("")
+    
     print(opção_1)
     print(opção_2)
+    
     escolha = input("Digite aqui a opção que deseja: ")
+    
     print("")
+    
     while escolha != "A" and escolha != "B": 
+    
         print("Opção inválida, digite A ou B.")
         escolha = input("Digite aqui a opção que deseja: ")
+        
     if escolha == "B":
         print("VOCÊ: Que diabos está acontecendo e como assim você é um"
            " espectro invisível de outra dimensão?! Eu tô ficando maluco?!")
+        
         print("")
+        
         print("KITARA: não há tempo para explicações Insperiano! Você precisa"
           "adiar o EP1 para o bem de todos! Aliás, qual o seu nome?")
-    if escolha == "A": 
-        print("Essa não, você correu para rua e morreu atropelado!")
-        jogo = False
-    
+        
         nome_do_jogador = input("Digite seu nome: ")
+        print("")
+        
         print("KITARA: Certo {},"
       " precisamos encontrar seu professor!".format(nome_do_jogador))
+        
+    elif escolha == "A": 
+        print("Essa não, você correu para rua e morreu atropelado!")
+        jogo = False
+    elif jogo == False: 
+        break
+    
+print(Vitória)
