992f6bc22baea3dceaf101e7c0f1b4504ca3068a
Byron-Hans <37991863+Byron-Hans@users.noreply.github.com>
2019-04-21 18:13:57 -0300

    Mano, seguinte, eu adicionei mais alguns cenários, variáveis, e fui elaborando a história, adicionei uma luta, mais uma possível morte, conceito de vida, e entre outras, tem umas coisas de estética que eu queria mudar, tá faltando uns espaçamentos, dá pra fazer isso dps, quando a gente for revisar, por enquanto vamos elaborar a história, não pode mudar o formato do dicionário de cenários, senão caga todo o código, quando for colocar um cenário novo é só adicionar lá no dicionario, tinha mais coisas que eu queria comentar mas eu esqueci ;-;

--- ep1.py
+++ ep1.py
@@ -1,78 +1,45 @@
 ﻿# EP 2019-1: Escape Insper
 #
-# Alunos: 
+# Alunos:
 # - aluno A: Marcelo Miguel, marcelocm8@al.insper.edu.br
 # - aluno B: Byron Hans, byronhboe@al.insper.edu.br
 
-jogo = True
-derrota = "GAME OVER! x___x "
+
 #definição de todos os cenários
 
-Hp=100
-def procurar_cenários(): 
-    cenário = {
-        "Prédio 1":{ 
-            "titulo":"Predio 1",
-            "descrição":"Você está no Predio 1, aonde deseja ir?",
-            "opções": {
-                "Elevador":"Entrar no Elevador",
-                "Biblioteca":"Entrar na Biblioteca"
-            }
-        },
-        "Elevador":{
-            "titulo": "Elevador",
-            "descrição":"Você está no elevador, selecione um andar:",
-            "opções": {
-                "-1":"Ir para o Techlab",
-                "4":"Ir para o andar das engenharias",
-                "5":"Ir para o refeitorio",
-            }
-        },
-        "-1":{
-            "Titulo": "Techlab",
-            "descrição":"Você está no Techlab, o habitat natural dos engenheiros"
-                        "Um veterano do 9 semestre FULL PISTOlA te desafiou para um combate"
-                        "Veterano(Ataque=20, Hp= 20)",
-            "opções":{
-                    "Atacar":"Atacar o veterano",
-                    "Elevador":"voltar para o Elevador",
-        },
-        "4":{
-            "Titulo": "Sala das Engenharias",
-            "descrição":"Você está no quarto andar, o andar que habita o Raul"
-                        "será que você é capaz de desafiar ele para uma batalha?"
-                        "KITARA: Insperiano!, devo te alertar que, antes de chamar o Raul pro fight"
-                        "tente passar no -1 andar primeiro"
-                        "*Raul aparece*"
-                        "Raul e seu assistente tão tudo cansado de ter corrigido as PI"
-                        "Parece ser a hora perfeita para conseguir atrasar o EP",
-        },
-        "5":{
-        }
-        
-                
-    }
-    return cenário
+cenário = {
+        "Prédio 1":"Você está em frente ao prédio 1, o que deseja fazer?",
+        "Elevador":"Você está no elevador, selecione um andar:",
+        "Saguão":"Vocês tá no saguão do prédio 1, o que deseja fazer?", 
+        "Biblioteca":"Você está na biblioteca do olhar penetrante"}
+
 
 
 #início do código do jogo
-
+vida = 100
+vida_boss = 1000
+import time
+i = 0 #contador para linha do título
 jogo = True #o jogo funciona enquanto for true 
-derrota = "GAME OVER! x__x "
-Vitória = "Parabéns! Você venceu! Agora vá fazer o EP1! ¯\_(ツ)_/¯"
+derrota = "GAME OVER! x__x " 
+Vitória = "Parabéns! Você venceu! Agora vá fazer o EP1!¯\_(ツ)_/¯"
 
 while jogo == True:
     
     print("KITARA: Olá Insperiano, meu nome é Kitara, sou um espectro"
-      " invisível vindo de outra de dimensão. Fui enviada para porque você"
-      " precisa conseguir adiar a entrega do EP1 de Design de Software senão"
+      " invisível vindo de outra de dimensão. Fui enviada porque você"
+      " precisa conseguir adiar a entrega do EP1 de Design de Software"
       " senão você e todos os seus colegas vão pegar DP nessa matéria!!!"
       " Ficarei com você até conseguir, só então poderei voltar para minha"
-      "dimensão!")
+      " dimensão!")
+    time.sleep(1)
     
     print("")
     
     print(cenário["Prédio 1"])
+    i = len(cenário["Prédio 1"])
+    print(i*"-")
+    time.sleep(2)
     
     opção_1 = "A: Sair correndo com medo da Kitara."
     opção_2 = "B: Pedir para Kitara explicar melhor quem ela é."
@@ -80,36 +47,422 @@ while jogo == True:
     print("")
     
     print(opção_1)
+    time.sleep(1)
     print(opção_2)
+    time.sleep(3)
     
     escolha = input("Digite aqui a opção que deseja: ")
     
     print("")
     
-    while escolha != "A" and escolha != "B": 
-    
+    while escolha != "A" and escolha != "B":
+        time.sleep(2)
         print("Opção inválida, digite A ou B.")
         escolha = input("Digite aqui a opção que deseja: ")
         
-    if escolha == "B":
+    if escolha == "A":
+        time.sleep(2)
+        print("Essa não, você correu para rua e morreu atropelado!")
+        jogo = False
+    elif jogo == False:
+        time.sleep(2)
+        break
+        
+    elif escolha == "B":
+        time.sleep(2)
+        print("")
         print("VOCÊ: Que diabos está acontecendo e como assim você é um"
            " espectro invisível de outra dimensão?! Eu tô ficando maluco?!")
         
         print("")
+        time.sleep(2)
         
-        print("KITARA: não há tempo para explicações Insperiano! Você precisa"
-          "adiar o EP1 para o bem de todos! Aliás, qual o seu nome?")
-        
+        print("KITARA: Não há tempo para explicações Insperiano! Você precisa"
+          " adiar o EP1 para o bem de todos! Aliás, qual o seu nome?")
+        time.sleep(2)
         nome_do_jogador = input("Digite seu nome: ")
         print("")
+        time.sleep(2)
         
         print("KITARA: Certo {},"
       " precisamos encontrar seu professor!".format(nome_do_jogador))
         
-    elif escolha == "A": 
-        print("Essa não, você correu para rua e morreu atropelado!")
-        jogo = False
-    elif jogo == False: 
-        break
+        print("")
+        time.sleep(2)
     
-print(Vitória)
+        print(cenário["Prédio 1"])
+        i = len(cenário["Prédio 1"])
+        print(i*"-")
+        time.sleep(2)
+        
+        opção_1 = "A: Ir para o prédio 2."
+        opção_2 = "B: Ir para o saguão do prédio 1."
+        
+        print(opção_1)
+        time.sleep(1)
+        print(opção_2)
+        time.sleep(1)
+        
+        escolha = input("Digite aqui a opção que deseja: ")
+        print("")
+        while escolha != "A": #obrigar o jogador a passsar por A controle de input
+            if escolha == "B":
+                time.sleep(2)
+                print('')
+                print("VOCÊ: O Raul deve estar no prédio 2, acho melhor ir"
+                      " para lá...")
+                time.sleep(2)
+                print('')
+                escolha = input("Digite aqui a opção que deseja: ")
+            else:
+                print("Opção inválida, digite A ou B.")
+                time.sleep(2)
+                print("")
+                escolha = input("Digite aqui a opção que deseja: ")
+                print('')
+        
+                                
+        if escolha == "A":
+            time.sleep(2)
+            print('')
+            print("Ao tentar atravessar a rua para ir ao prédio 2, uma força"
+                  " magnética te joga com tudo para trás!"
+                  " As pessoas ao redor de te olham feio brevemente e tornam"
+                  " a seguir seus caminhos novamente.")
+            print("")
+            time.sleep(2)
+            print("KITARA: essa não, forças inimigas interdimensionais estão"
+                  " tentando nos impedir! Você não vai conseguir acessar"
+                  " o prédio 2 por enquanto! Vamos para o saguão!")
+            print("")
+            time.sleep(2)
+            print("VOCÊ: Isso tudo é uma loucura Eu só posso estar sonhando,"
+                  " mas vamos lá!")
+            print("")
+            time.sleep(3)
+        
+        print(cenário["Saguão"])
+        i = len(cenário["Saguão"])
+        print(i*"-")
+        print('')
+        
+        
+        opção_1 = "A: ir para a biblioteca."
+        time.sleep(1)
+        opção_2 = "B: ir para o elevador."
+        
+        
+        print(opção_1)
+        time.sleep(2)
+        print(opção_2)
+        time.sleep(2)
+        
+        
+        escolha = input("Digite aqui opção que deseja: ")
+        time.sleep(2)
+        
+        while escolha != "A" and escolha != "B": 
+            time.sleep(2)
+            print("Opção inválida, digite A ou B.")
+            time.sleep(2)
+            print("")
+            escolha = input("Digite aqui a opção que deseja: ")
+            time.sleep(2)
+            
+        
+        if escolha == "B":  
+            print("Você tenta acessar os eladores mas novamente uma força"
+                  "magnética te joga para trás com tudo!")
+            print('')
+            time.sleep(2)
+            print("VOCÊ: denovo isso! Ai que dor!")
+            time.sleep(2)
+            print('')
+            print("As pessoas te encaram brvevemte e não esboçam nenhuma outra"
+                  " reação apesar do que aconteceu")
+            time.sleep(2)
+            print('')
+            print("VOCÊ: Kitara, por que ninguém está reagindo a tudo isso?"
+                  " EU estou invisível também?")
+            time.sleep(2)
+            print('')
+            print("KITARA: Não, não está, repare que muitas delas te encaram,"
+                  " pelas energias que eu estou sentindo, elas estão possuidas"
+                  " pelas mesmas forças malignas interdimensionais que criam"
+                  " os campos magnéticos que te impedem de entrar em"
+                  " determinados lugares.")
+            time.sleep(2)
+            print('')
+            print("VOCÊ: pera aí, você consegue sentir os campos magnéticos e"
+                  " as energias malignas?")
+            time.sleep(2)
+            print('')
+            print("KITARA: Sim hahaha, desculpe não te avisar, é que...")
+            time.sleep(1)
+            print('')
+            print("VOCÊ: Nossa mano, se eu te enxergasse te deitava na" 
+                  " porrada!")
+            print('')
+            time.sleep(2)
+            print("KITARA: HAHAHAH, sem mais delongas, vamos para a" 
+                  " biblioteca! Parece um lugar menos perigoso...")
+            print('')
+            time.sleep(2)
+        
+        print('')
+        print("Você está na entrada da biblioteca e novamente as pessoas"
+              " te olham feio brevemente antes de retomar o que faziam.")
+        print('')
+        time.sleep(2)
+        print("Você percebe que tem um veterano te que conitnuo te encarando.")
+        
+        opção_1 = "A: encarar de volta com a testa franzida."
+        opção_2 = "B: desviar o olhar."
+        
+        print(opção_1)
+        time.sleep(1)
+        print(opção_2)
+        time.sleep(1)
+        
+        escolha = input("Digite aqui o deseja fazer: ")
+        
+        while escolha != "A" and escolha != "B": #das duas formas o veterano ataca
+            time.sleep(2)
+            print("Opção inválida, digite A ou B.")
+            time.sleep(2)
+            print("")
+            escolha = input("Digite aqui a opção que deseja: ")
+            time.sleep(2)
+            print("")
+        
+        if escolha == "A":
+            print ("KITARA: Ele é esta possuido," 
+                   " cuidado {}!".format(nome_do_jogador))
+        
+        print('')    
+        print("O veterano começa a andar na sua direção com um olhar mortal.")
+        time.sleep(2)
+        print('')    
+        print("KITARA: essa não! Ele vai está possuido e vai nos atacar!")
+        
+        opção_1 = "A: Correr. "
+        opção_2 = "B: Encará-lo com um olhar firme"
+        
+        print(opção_1)
+        time.sleep(1)
+        print(opção_2)
+        time.sleep(1)
+        print('')
+        
+        escolha = input("Digite aqui a opção que deseja: ")
+        
+        
+        while escolha != "A" and escolha != "B":
+            time.sleep(2)
+            print("Opção inválida, digite A ou B.")
+            time.sleep(2)
+            print("")
+            escolha = input("Digite aqui a opção que deseja: ")
+            time.sleep(2)
+            print("")
+        
+        
+        if escolha == "A":
+            print('')
+            print ("KITARA: Nãooo! Não corra, ele se você correr você morre!")
+            print('')
+            time.sleep(2)
+            print("O veterano te alcançou, entrou dentro do seu corpo,"
+                  " consumiu sua alma e você caiu duro, como se fosse um"
+                  " infarto fulminante.")
+            print('')
+            jogo = False
+        
+        elif jogo == False:
+            break
+        
+        else:
+            print(" O veterano  olhou bem no fundo dos seus olhos e te levou"
+                  " para uma outra dimensão. Vocês estavam num campo de"
+                  " batalha!")
+            print('')
+            time.sleep(2)
+            print("VOCÊ: Meu Deus! Onde eu estou?! Como eu vim parar aqui?!")
+            print('')
+            time.sleep(2)
+            print("KITARA: essa não, o veterano possuido de te invocou para"
+                  " uma dimensão de guerra! Você terá que lutar com ele!")
+            print('')
+            time.sleep(2)
+            print("VOCÊ: como assim eu vou ter que lutar?!")
+            print('')
+            time.sleep(2)
+            print("KITARA: Você faz muitas perguntas! Controle o medo!"
+                  " Preste bem atenção você tem 100 pontos de vida"
+                  " se você perder esses 100 pontos, vc morre e estará tudo"
+                  " acabado! Não deixei o seu inimigo te acertar!"
+                  " Nessa dimensão eu tenho o poder de forjar um único"
+                  " equipamento para você lutar!")
+            
+            opção_1  = "A: Espada divina - Causa 700 de dano"
+            opção_2  = "B: Escudo divino - Reflete até 500 de dano"
+            
+            print(opção_1)
+            time.sleep(1)
+            print(opção_2)
+            time.sleep(1)
+            print('')
+            
+            
+            escolha  = input("Digite aqui a opção que deseja: ")
+            
+            
+            while escolha != "A" and escolha != "B": #das duas formas o veterano ataca
+                time.sleep(2)
+                print("Opção inválida, digite A ou B.")
+                time.sleep(2)
+                print("")
+                escolha = input("Digite aqui a opção que deseja: ")
+                time.sleep(2)
+                print("")
+                    
+            print ("KITARA: agora que você escolheu seu equipamento,"
+                   "lute como um bravo guerreiro!")
+            time.sleep(2)
+            print('')
+            print("VOCÊ: Meu Deus do céu que coisa maluca mas"
+                  " vamos lá, por Demaciaa!!!")
+            print('')
+            time.sleep(2)
+            
+            if escolha == "A":
+                print("Prepare-se para o combate!")
+                time.sleep(2)
+                print('')
+                print("{} VERSUS O Veterano Sugador" 
+                      " de Almas".format(nome_do_jogador))
+                time.sleep(2)
+                print('')
+                print("HP - {}: ".format(nome_do_jogador))
+                print(vida)
+                print('')
+                print("HP - O Veterando Sugador de Almas: ")
+                print(vida_boss)
+                print('')
+                print('')
+                time.sleep(3)
+                print("3")
+                time.sleep(1)
+                print("2")
+                time.sleep(1)
+                print("1")
+                time.sleep(1)
+                print('')
+                print("FIGHT")
+                print('')
+                time.sleep(2)
+                print("***CLAAASH***")
+                print('')
+                vida -= 525
+                vida_boss -= 700
+                print("Dano recebido: 525")
+                print("Dano causado: 700")
+                print('')
+                print("HP - {}: ".format(nome_do_jogador))
+                print(vida)
+                print('')
+                print("HP - O Veterando Sugador de Almas: ")
+                print(vida_boss)
+                print('')   
+                jogo = False
+            
+            elif jogo == False:
+                break
+            
+            if escolha == "B":
+                print("Prepare-se para o combate!")
+                time.sleep(2)
+                print('')
+                print("{} VERSUS O Veterano Sugador" 
+                      " de Almas".format(nome_do_jogador))
+                time.sleep(2)
+                print('')
+                print("HP - {}: ".format(nome_do_jogador))
+                print(vida)
+                print('')
+                print("HP - O Veterando Sugador de Almas: ")
+                print(vida_boss)
+                print('')
+                print('')
+                time.sleep(3)
+                print("3")
+                time.sleep(1)
+                print("2")
+                time.sleep(1)
+                print("1")
+                time.sleep(1)
+                print('')
+                print("FIGHT")
+                print('')
+                time.sleep(2)
+                print("***CLAAASH***")
+                print('')
+                vida -= 25
+                vida_boss -= 500
+                print("Dano recebido: 25")
+                print("Dano causado: 500")
+                print('')
+                print("HP - {}: ".format(nome_do_jogador))
+                print(vida)
+                print('')
+                print("HP - O Veterando Sugador de Almas: ")
+                print(vida_boss)
+                print('')    
+                
+                #segundo ataque
+                
+                time.sleep(2)
+                print('')
+                print("HP - {}: ".format(nome_do_jogador))
+                print(vida)
+                print('')
+                print("HP - O Veterando Sugador de Almas: ")
+                print(vida_boss)
+                print('')
+                print('')
+                time.sleep(3)
+                print("3")
+                time.sleep(1)
+                print("2")
+                time.sleep(1)
+                print("1")
+                time.sleep(1)
+                print('')
+                print("FIGHT")
+                print('')
+                time.sleep(2)
+                print("***CLAAASH***")
+                print('')
+                vida -= 25
+                vida_boss -= 500
+                print("Dano recebido: 25")
+                print("Dano causado: 500")
+                print('')
+                print("HP - {}: ".format(nome_do_jogador))
+                print(vida)
+                print('')
+                print("HP - O Veterando Sugador de Almas: ")
+                print(vida_boss)
+                print('')      
+                
+                
+                
+                
+            
+                
+                
+
+    
+print(derrota) 
+
+    
